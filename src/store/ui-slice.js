06137cf1cc8b3a13daf6f536fa8bbaed5f4849c1
rogofan <sacokov@gmail.com>
2023-02-26 20:32:49 +0100

    First commit

---
+++ src/store/ui-slice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState: { cartVisible: false, notification: null },
+  reducers: {
+    toggle(state) {
+      state.cartVisible = !state.cartVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
+});
+
+export const uiActions = uiSlice.actions;
+export default uiSlice;
